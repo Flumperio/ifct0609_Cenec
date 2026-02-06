6758c9eb4ba43ec67b95b511caa4928a307ea2ce
Juan Carlos <jcs0969@icloud.com>
2026-02-06 11:16:32 +0100

    Update Impresion_Resultados.java

--- Ejercicios/Java/ProgramacionSistemas/src/Metodos/Impresion_Resultados.java
+++ Ejercicios/Java/ProgramacionSistemas/src/Metodos/Impresion_Resultados.java
@@ -2,24 +2,19 @@ package Metodos;
 
 public class Impresion_Resultados {
     public void cabecera (String nombre){
-        //Definicion variables
-        int long_nombre = nombre.length();
-        int long_total = 20;
-        if (long_nombre % 2 == 0)
-            long_total += 1;
-        for (int cnt = 0; cnt < long_total; cnt ++)
-            System.out.print("*");
-        System.out.println("");
-        for (int cnt = 0; cnt < ((long_total-long_nombre)/2) - 1; cnt++){
-            System.out.print("*");
-        }
-        System.out.print(" " + nombre + " ");
-        for (int cnt = 0; cnt < ((long_total-long_nombre)/2); cnt++){
-            System.out.print("*");
-        }
+        int len = nombre.length();
+        int total = 20;
+        if (len % 2 == 0) total++;  // Ajuste paridad
+        // Línea superior
+        for (int i = 0; i < total; i++) System.out.print("*");
         System.out.println();
-        for (int cnt = 0; cnt < long_total; cnt ++)
-            System.out.print("*");
-        System.out.println("");
-    }
+        // Línea centrada
+        int espacios = (total - len) / 2;
+        for (int i = 0; i < espacios; i++) System.out.print("*");
+        System.out.print(" " + nombre + " ");
+        for (int i = 0; i < total - espacios - len - 1; i++) System.out.print("*");
+        System.out.println();
+        // Línea inferior
+        for (int i = 0; i < total; i++) System.out.print("*");
+        System.out.println();
 }
